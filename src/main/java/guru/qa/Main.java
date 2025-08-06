0ffce20dfcab133df870eb29dd2bb0c1fee69007
Vasili6666 <w2e3r4t51982@gmail.com>
2025-08-06 20:21:37 +0200

    add time 20:21

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,11 +1,9 @@
 package guru.qa;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
             System.out.println("Hello, Guru 2 !");
         }
     }
-// изменние в ветке main 20:17
+// изменние в ветке additional branch 20:21
